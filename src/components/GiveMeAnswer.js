03e38b1d91d2c04e35e3aef98dd1d1d7565d10a0
Ján Vlčko <sinon0159@gmail.com>
2021-11-16 23:07:19 +0100

    last change

--- src/components/GiveMeAnswer.js
+++ src/components/GiveMeAnswer.js
@@ -10,24 +10,46 @@ function GiveMeAnswer() {
     answer3: "",
     answer4: "",
   });
+  //prepinanie zobrazenie komponentu
+  const [show, setShow] = useState(false);
+  //konecny string po uprave
+  const [inOneString, setInOneString] = useState();
 
-  //moznost dva spravis vseobecnu funkciu, ktosu posles kazdemu inputu/componentu
+  //vseobecna funkcia, ktosu posles kazdemu inputu/componentu
   const handleChangeStates = (value) => {
+    //pridavas nove hodnoty do state
     setAnswers((prev) => {
       return {
         //... su spread operator ktory rozlozi object na key: value,
         //pretoze to menime v state
+
+        //predosly state
         ...prev,
+        //novym state prepises ten stary + ostatne hodnoty ti ostanu
         ...value,
       };
     });
   };
 
-  let text = [];
+  //toggle pre show/hidden
+  const hadleShow = () => {
+    setShow(!show);
+    setInOneString(createString());
+  };
+
+  //tu si vytvarame jeden string zo vsetkych odpovedi
+  const createString = () => {
+    const string = Object.values(answers).reduce((acc, curr, i) => {
+      if (curr === "") return;
+
+      return `${acc} ${curr}`;
+    });
+    return string;
+  };
+
   return (
     <Fragment>
-      <div style={{ backgroundColor: "snow" }}>
-        <p>best varian :) don't repeat yourself</p>
+      <div style={{ marginTop: 50 }}>
         <Question
           getStates={handleChangeStates}
           rank="answer1"
@@ -49,11 +71,8 @@ function GiveMeAnswer() {
           question="Otazka 4"
         />
       </div>
-      <button>Show answer</button>
-      {Object.values(answers).map((answer, i) => {
-        text.push(`${answer},`);
-      })}
-      <h2>{text}</h2>
+      <button onClick={hadleShow}>Show answer</button>
+      {show && <h2>{inOneString}</h2>}
     </Fragment>
   );
 }
